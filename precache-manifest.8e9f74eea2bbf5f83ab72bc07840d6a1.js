52797afda897f4e7fc2e4d58f64027213d6341b6
Aleksey <aleksey_naumenko@ukr.net>
2020-03-11 20:38:39 +0200

    Updates

---
+++ precache-manifest.8e9f74eea2bbf5f83ab72bc07840d6a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a78fecd60e8ced45360a672d921739",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "2a54a0dbfb15cf61fbfa",
+    "url": "/todolist-react/static/css/main.a95c8360.chunk.css"
+  },
+  {
+    "revision": "783e93f25a38f90ca150",
+    "url": "/todolist-react/static/js/2.c7783d26.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/todolist-react/static/js/2.c7783d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a54a0dbfb15cf61fbfa",
+    "url": "/todolist-react/static/js/main.0d5aeda2.chunk.js"
+  },
+  {
+    "revision": "a5b2ad1293b4a050efdd",
+    "url": "/todolist-react/static/js/runtime-main.03878e3d.js"
+  }
+]);
